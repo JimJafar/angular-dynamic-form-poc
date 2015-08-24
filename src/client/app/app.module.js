de5d017f8c8edf1b0b2f285743984819f95b2ba3
Jim Sangwine <jim@competa.com>
2015-08-24 13:11:22 +0200

    Removed unneeded dependencies and added ngMessages

--- src/client/app/app.module.js
+++ src/client/app/app.module.js
@@ -4,9 +4,7 @@
     angular.module('app', [
         'app.core',
         'app.widgets',
-        'app.admin',
-        'app.dashboard',
-        'app.layout'
+        'ngMessages'
     ]);
 
 })();
